464b366f236c3325596727d53f4300b501823fa6
Marius Årnes <marius.appa@gmail.com>
2020-05-16 20:22:30 +0200

    added CategoriesController.cs

--- Provitamin.Api/Controllers/CategoriesController.cs
+++ Provitamin.Api/Controllers/CategoriesController.cs
@@ -8,38 +8,38 @@ namespace Tita_Api.Controllers
 {
     
     [Route("api/[controller]/[action]")]
-    public class BrandsController : ControllerBase
+    public class CategoriesController : ControllerBase
     {
-        private readonly IBrandRepository _brandRepository;
-        public BrandsController(IBrandRepository brandRepository)
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoriesController(ICategoryRepository categoryRepository)
         {
-            _brandRepository = brandRepository;
+            _categoryRepository = categoryRepository;
         }
         [HttpGet(Name ="get")]
-        public async Task<ActionResult<List<Brand>>>  Get()
+        public async Task<ActionResult<List<Category>>>  Get()
         {
-            return await _brandRepository.Get();
+            return await _categoryRepository.Get();
         } 
         
         [HttpPost( Name = "create")]
-        public async Task<ActionResult<Brand>> Create([FromBody]Brand brand)
+        public async Task<ActionResult<Category>> Create([FromBody]Category brand)
         {
             if (string.IsNullOrEmpty(brand.Title) )
                 return BadRequest();
-            _brandRepository.Create(brand);
+            _categoryRepository.Create(brand);
             return CreatedAtAction("Get", new { title = brand.Title}, brand);
         }
         
         [HttpDelete]
-        public async Task<IActionResult> Delete([FromBody]Brand brand)
+        public async Task<IActionResult> Delete([FromBody]Category brand)
         {
-            var product = await _brandRepository.Get(brand.Title);
+            var product = await _categoryRepository.Get(brand.Title);
 
             if (product == null)
             {
                 return NotFound();
             }
-            _brandRepository.Remove(brand.Title);
+            _categoryRepository.Remove(brand.Title);
             return NoContent();
         }
     }
--- Provitamin.Api/Controllers/ProductsController.cs
+++ Provitamin.Api/Controllers/ProductsController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Common.Interfaces.Repositories;
 using Domain.Entities.Product;
