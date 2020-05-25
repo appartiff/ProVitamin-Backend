683ab5ba24993da126ffdfe0f615e77936c210e2
Marius Årnes <marius.appa@gmail.com>
2020-05-25 23:15:49 +0200

    GetProductQuery.cs

---
+++ Application/Products/Queries/GetProduct/GetProductHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces.Repositories;
+using Domain.Entities.Product;
+using MediatR;
+
+namespace Application.Products.Queries.GetProduct
+{
+    public class GetProductHandler: IRequestHandler<GetProductQuery, Product>
+    {
+        private readonly IProductRepository _productRepository;
+        public GetProductHandler( IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public async Task<Product> Handle(GetProductQuery request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.Get(request.Id);
+            if (product == null)
+                throw new NotFoundException(nameof(product), request);
+            return product;
+        }
+    }
+}
---
+++ Application/Products/Queries/GetProduct/GetProductQuery.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities.Product;
+using MediatR;
+
+namespace Application.Products.Queries.GetProduct
+{
+    public class GetProductQuery :IRequest<Product>
+    {
+        public string Id { get; set; }
+    }
+}
