111966bb69d6a243c0ea20e07ef5a8a654ebcf95
Marius Årnes <marius.appa@gmail.com>
2020-05-25 23:15:32 +0200

    createProduct command

---
+++ Application/Products/Commands/CreateProductCommand.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities.Product;
+using MediatR;
+
+namespace Application.Products.Commands
+{
+    public class CreateProductCommand : IRequest<string>
+    {
+        public string Sku { get; set; }
+        public ProductPricing Pricing { get; set; }
+        public ProductDetails Details { get; set; }
+        public ProductShipping Shipping { get; set; }
+    }
+}
---
+++ Application/Products/Commands/CreateProductCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces.Repositories;
+using Domain.Entities.Product;
+using MediatR;
+
+namespace Application.Products.Commands
+{
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, string>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public CreateProductCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            
+        }
+        public async Task<string> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new Product()
+            {
+                Sku = request.Sku,
+                Details = request.Details,
+                Pricing = request.Pricing,
+                Shipping = request.Shipping
+            };
+            await _productRepository.Create(entity,cancellationToken);
+            return entity.Id;
+        }
+    }
+}
